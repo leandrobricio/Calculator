7bffbf83958e2287a982a7cf5859efe0f5cf6fc2
leandrobricio <leandrobriciodafontejunior@gmail.com>
2024-02-23 17:44:44 -0300

    commit final

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using exemplos_fundamentos.Models;
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(10, 30);
+calc.Subtrair(10,50);
+calc.Multiplicar(15,45);
+calc.Dividir(2,2);
+calc.Potencia(3,3);
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30);
+calc.RaizQuadrada(9);
+
+int numero = 10;
+
+Console.WriteLine(numero);
+
+Console.WriteLine("Incrementando o 10");
+numero++;
+
+Console.WriteLine(numero);
